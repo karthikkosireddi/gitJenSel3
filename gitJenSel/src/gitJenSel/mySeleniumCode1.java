c28482a09aea9a1eb430e2fe52e556fc1de61a6c
kk-git <kk@varkasa.com>
2020-06-26 14:07:54 -0700

    added wait n print

--- gitJenSel/src/gitJenSel/mySeleniumCode1.java
+++ gitJenSel/src/gitJenSel/mySeleniumCode1.java
@@ -5,8 +5,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class mySeleniumCode1 {
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws Exception {
+		System.out.println("      >>>>>>>>> Started Execution");
 		WebDriver myDriver;
 		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 		myDriver = new ChromeDriver(); 
@@ -15,6 +15,9 @@ public class mySeleniumCode1 {
 		myDriver.navigate().to("https://www.itelearn.com/");
 		
 
+		Thread.sleep(4000);
+		myDriver.quit();
+		System.out.println("      >>>>>>>>> Ended Execution");
 	}
 
 }

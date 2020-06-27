51566ef67492887fda13447da79802c8682775b4
kk-git <kk@varkasa.com>
2020-06-26 19:08:03 -0700

    change

--- gitJenSel/src/gitJenSel/mySeleniumCode1.java
+++ gitJenSel/src/gitJenSel/mySeleniumCode1.java
@@ -1,7 +1,11 @@
 package gitJenSel;
 
+import java.net.URL;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class mySeleniumCode1 {
 
@@ -10,6 +14,13 @@ public class mySeleniumCode1 {
 		WebDriver myDriver;
 		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 		myDriver = new ChromeDriver(); 
+		
+		//DesiredCapabilities capability = DesiredCapabilities.chrome();
+		// capability.setCapability("jenkins.label","redhat5 && amd64");
+		// capability.setCapability("jenkins.nodeName","(master)");
+		//WebDriver myDriver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capability);
+		
+		
 		myDriver.manage().window().maximize(); 
 		
 		myDriver.navigate().to("https://www.itelearn.com/");

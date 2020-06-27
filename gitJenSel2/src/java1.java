0ee02c5a7ae6635b8dbc4deca275b399e4fe7074
kk-git <kk@varkasa.com>
2020-06-26 19:01:08 -0700

    added a simple java code

---
+++ gitJenSel2/src/java1.java
@@ -0,0 +1,15 @@
+
+public class java1 {
+
+	public static void main(String[] args) {
+		System.out.println("Start Java1");
+
+		int x = 4, y = 65;
+		int z = x + y;
+		
+		System.out.println("Value of Z is " + z);
+		System.out.println("End Java1");
+
+	}
+
+}
